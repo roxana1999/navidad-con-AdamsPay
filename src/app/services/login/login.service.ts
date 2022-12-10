b52209f4d7eec8dcdf0adee17a22420b8a63685b
Roxana Fernández <roxana.jacquelinefa@fpuna.edu.py>
2022-12-10 00:13:41 -0300

    Se añadió el inicio de sesión con google

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -5,6 +5,8 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { vaciarCarrito } from 'src/app/components/mi-carrito/functions/vaciarCarrito';
 
+import jwt_decode from "jwt-decode";
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,16 +16,23 @@ export class LoginService {
   constructor(private router: Router) { }
 
   login(email: string, password: string){
-    firebase.auth().signInWithEmailAndPassword(email, password).then(
-      response => {
-        firebase.auth().currentUser?.getIdToken().then(
-          token => {
-            localStorage.setItem('UserToken',token);
-            this.router.navigate(['/']);
-          }
-        )
+    // firebase.auth().signInWithEmailAndPassword(email, password).then(
+    //   response => {
+    //     firebase.auth().currentUser?.getIdToken().then(
+    //       token => {
+    //         localStorage.setItem('UserToken',token);
+    //       }
+    //     )
+    //   }
+    // )
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(
+      result => {
+        const credential = result.credential as firebase.auth.OAuthCredential;
+        const token = credential.idToken;
+        localStorage.setItem('UserToken', token!);
+        this.router.navigate(['/']);
       }
-    )
+    );
   }
 
   logout(){
